48697eb87add9df57b322ebff0662ea865f738cb
Gautier DJOSSOU <gautier.djossou@africadigitalizer.com>
2026-05-16 20:49:48 +0100

    correction sur le pdf du cv

--- resources/views/pdf/cv_template.blade.php
+++ resources/views/pdf/cv_template.blade.php
@@ -125,7 +125,7 @@
             color: #888888;
             font-weight: 700;
             font-size: 8pt;
-            margin-bottom: 2px;
+            margin-bottom: 0.25rem;
         }
 
         .skills-item {
